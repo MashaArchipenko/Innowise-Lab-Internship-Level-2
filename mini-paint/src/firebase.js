db1e470c746e8b77023d378c0856464e568f099e
MashaArchipenko <masha.archipenko.99@mail.ru>
2021-03-28 00:58:55 +0300

    setting of project

--- mini-paint/src/firebase.js
+++ mini-paint/src/firebase.js
@@ -1,11 +1,7 @@
 import firebase from 'firebase'
 import 'firebase/auth'
-import dotenv from  'dotenv'
-import path from 'path'
-console.log(process.env.REACT_APP_API_KEY)
 const fire =  firebase.initializeApp(
 {
-    
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
